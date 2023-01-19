2e709ea38af33db3b9480a3dd94c2892d94519ef
marquin.e <marcos.emanuel.04@gmail.com>
2023-01-19 20:08:50 -0300

    JS para registrar o dia no localStorage do navegador

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
-const form = document.querySelector("#form-habits")
-const nlwSetup = new NLWSetup(form)
+const form = document.querySelector("#form-habits") // Recebendo o elemento form
+const nlwSetup = new NLWSetup(form) // Instanciando a biblioteca
+const button = document.querySelector("header button") // Recebendo o elemento button que está dentro do header
 
-const data = {
-  run: ["01-01", "01-02", "01-06", "01-07", "01-08", "01-09"],
-  takePills: ["01-04", "01-05", "01-06", "01-10"],
-  water: ["01-02", "01-05", "01-06", "01-07"],
+button.addEventListener("click", add) // Busca o clique no botão
+form.addEventListener("change", save) // Busca alterações no form
+
+function add() {
+  // Pega o dia atual em formato DD/MM, verifica se ele existe, caso não ele adiciona ao form
+  today = new Date().toLocaleDateString("pt-br").slice(0, -5)
+  const dayExist = nlwSetup.dayExists(today)
+  if (dayExist) {
+    alert("O dia já existe! 🔴")
+  } else {
+    nlwSetup.addDay(today)
+    alert("O dia foi adicionado! ✅")
+  }
 }
 
-nlwSetup.setData(data)
+function save() {
+  // Função para salvar os dados no localStorage do navegador e converte para JSON
+  localStorage.setItem('NLWSetup@habits', JSON.stringify(nlwSetup.data))
+
+}
+
+// Converte de JSON para objeto
+const data = JSON.parse(localStorage.getItem('NLWSetup@habits')) || {} // Caso não exista nada, ele registra um objeto vazio
+nlwSetup.setData(data) // Salva as informações
 nlwSetup.load()
